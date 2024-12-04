8b4e74a78ed458cb9656d84b2b6188edbe513d2f
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2024-12-04 17:27:26 +0800

    fs

--- swift/test1/Landmarks/Landmarks/Views/LandmarkList.swift
+++ swift/test1/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -9,26 +9,48 @@ import SwiftUI
 
 
 struct LandmarkList: View {
-    @State var landmarkList: [Landmark] = landmarks
+    @State private var landmarkList: [Landmark] = landmarks
+    @State private var showFavoritesOnly: Bool = false
+    var filteredLandmarks: [Landmark] {
+        landmarkList.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
 
     var body: some View {
-        Button(action: {
-            if landmarkList.count > 0 {
-                landmarkList.remove(at: 0)
+
+        VStack() {
+            HStack {
+                // del
+                Button(action: {
+                    print("del button clicked")
+                    if landmarkList.count > 0 {
+                        landmarkList.remove(at: 0)
+                    }
+                }, label: {
+                    Text("del").foregroundColor(.red)
+                }).background(Color.blue).frame(height: 160)//.id("idDelFirst")
+                // filter
+                Button(action: {
+                    print("filter button clicked, showFavoritesOnly:")
+                    showFavoritesOnly = !showFavoritesOnly
+                }, label: {
+                    Text("filter=\(showFavoritesOnly.description)")
+                }).background(Color.orange).frame(height: 50).offset(x: 100)//.id("idFilterFavorite")
             }
-        }, label: {
-            Text("del")
-        })
-        NavigationSplitView {
-            List(landmarkList, id: \.id) {landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            NavigationSplitView {
+                List {
+                    ForEach(filteredLandmarks, id: \.id) { landmark in
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            LandmarkRow(landmark: landmark)
+                        }}
                 }
-            }.navigationTitle("Landmarks")
-        } detail: {
-            Text("Select a Landmark")
+                .navigationTitle("Landmarks")
+            } detail: {
+                Text("Select a Landmark")
+            }
         }
     }
 }
--- swift/test1/Landmarks/Landmarks/Views/LandmarkRow.swift
+++ swift/test1/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -18,7 +18,8 @@ struct LandmarkRow: View {
             Spacer()
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
-            }   
+                    .foregroundColor(.yellow)
+            }
         }
         
     }
