408f0f1a4935e1cc426f7ed10ad1eeceb69653c4
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2024-12-04 16:23:16 +0800

    fs

--- swift/test1/Landmarks/Landmarks/Views/LandmarkList.swift
+++ swift/test1/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 
 struct LandmarkList: View {
+    @State var landmarkList: [Landmark] = landmarks
+
     var body: some View {
+        Button(action: {
+            if landmarkList.count > 0 {
+                landmarkList.remove(at: 0)
+            }
+        }, label: {
+            Text("del")
+        })
         NavigationSplitView {
-            List(landmarks, id: \.id) {landmark in
+            List(landmarkList, id: \.id) {landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
