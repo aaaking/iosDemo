dec84d9d9a4161bf73d9d36a5b9cb54d3c4c9eec
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2024-12-04 13:45:27 +0800

    fs

--- swift/test1/Landmarks/Landmarks/ContentView.swift
+++ swift/test1/Landmarks/Landmarks/ContentView.swift
@@ -9,24 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image(systemName: "globe")
-            .imageScale(.large)
-            .foregroundStyle(.tint)
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .foregroundColor(Color.pink)
-                .padding(6.0)
-                .frame(width: nil, height: nil)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        //        Image(systemName: "globe")
+        //            .imageScale(.large)
+        //            .foregroundStyle(.tint)
+        VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y:-130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(Color.pink)
+                    .padding(6.0)
+                    .frame(width: nil, height: nil)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
