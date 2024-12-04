84a204f2bde33f1fc859b474e7ebd239bae8efd7
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2024-12-04 15:19:19 +0800

    sf

--- swift/test1/Landmarks/Landmarks/Model/ModelData.swift
+++ swift/test1/Landmarks/Landmarks/Model/ModelData.swift
@@ -19,13 +19,13 @@ func load<T: Decodable>(_ filename: String) -> T {
     do {
         data = try Data(contentsOf: file)
     } catch {
-        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+        fatalError("Couldn't load \(filename) from main bundle:\(error)")
     }
     
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
     } catch {
-        fatalError("Couldn't parse \(filename) as main \(T.self):\n\(error)")
+        fatalError("Couldn't parse \(filename) as main \(T.self):\(error)")
     }
 }
