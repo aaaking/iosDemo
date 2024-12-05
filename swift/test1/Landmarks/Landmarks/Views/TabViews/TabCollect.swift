d5b1dbba3406cdf3548642b32583a8a863cef5a9
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2024-12-05 15:21:09 +0800

    fs

--- swift/test1/Landmarks/Landmarks/Views/TabViews/TabCollect.swift
+++ swift/test1/Landmarks/Landmarks/Views/TabViews/TabCollect.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct TabCollect: View {
+    @State private var modelData = ModelData.sharedInstance()
+
     var body: some View {
-        Text("Collect!")
+        Group {
+//            ForEach(modelData.hikes, id: \.id) {hike in
+//                HikeView(hike: hike)
+//            }
+            HikeView(hike: modelData.hikes[0])
+//            HikeView(hike: modelData.hikes[1])
+//            HikeView(hike: modelData.hikes[2])
+//            HikeView(hike: modelData.hikes[3])
+//            for (index, hike) in modelData.hikes.enumerated() {
+//                HikeView(hike: hike)
+//            }
+        }
     }
 }
 
