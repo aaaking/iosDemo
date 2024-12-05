cf4e64c8e076db568c9ef954be873ec432af23ad
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2024-12-05 19:14:49 +0800

    fs

--- swift/test1/Landmarks/Landmarks/Views/PageView/FeatureCard.swift
+++ swift/test1/Landmarks/Landmarks/Views/PageView/FeatureCard.swift
@@ -12,14 +12,39 @@ struct FeatureCard: View {
 
 
     var body: some View {
-        ZStack() {
-            landmark.featureImage?
-                .resizable()
-            Text("\(landmark.id)")
-                .foregroundColor(.red)
-        }
+        landmark.featureImage?
+            .resizable()
+            .overlay {
+                TextOverlay(landmark: landmark)
+            }
     }
 }
+
+struct TextOverlay: View {
+    var landmark: Landmark
+    var gradient: LinearGradient {
+        .linearGradient(
+            Gradient(colors: [.black.opacity(0.6), .black.opacity(0)]),
+            startPoint: .bottom,
+            endPoint: .center)
+    }
+
+
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            gradient
+            VStack(alignment: .leading) {
+                Text("\(landmark.id)-\(landmark.name)")
+                    .font(.title)
+                    .bold()
+                Text(landmark.park)
+            }
+            .padding()
+        }
+        .foregroundStyle(.white)
+    }
+}
+
 
 
 #Preview {
