c7ce9a07aefda05d8317eed0f711cd4e1b1e0a42
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2024-12-05 18:35:38 +0800

    fs

--- swift/test1/Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
+++ swift/test1/Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -8,20 +8,35 @@
 import SwiftUI
 
 struct ProfileHost: View {
-    @Environment(\.editMode) var editModeMi
+    @Environment(\.editMode) var editMode
     @State private var modelData = ModelData.sharedInstance()
     @State private var draftProfile = Profile.default
 
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel"/*, role: .cancel*/) {
+                        print("testdraft cancel")
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
                 Spacer()
                 EditButton()
             }
-            if editModeMi?.wrappedValue == .inactive {
+            if editMode?.wrappedValue == .inactive {
                 ProfileSummary(profile: modelData.profile)
             } else {
                 ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        print("testdraft appear")
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        print("testdraft disappear")
+                        modelData.profile = draftProfile
+                    }
             }
         }
         .padding()
