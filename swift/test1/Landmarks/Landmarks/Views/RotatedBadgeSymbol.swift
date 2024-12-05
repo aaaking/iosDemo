fb26d7b60708c08b833a505be8c9d82233dc71ea
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2024-12-05 11:55:57 +0800

    fs

---
+++ swift/test1/Landmarks/Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by aaaking on 2024/12/5.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
+
